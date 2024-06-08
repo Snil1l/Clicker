8582c800f77c8d6b1385c03044172cc376b24f98
Ivan Shamin <ivansamin215@gmail.com>
2024-06-08 11:44:26 +0300

    sf

---
+++ js/index.js
@@ -0,0 +1,33 @@
+const count = document.querySelector('.count__click');
+const pepe = document.querySelector('.pepe');
+const btnBoost = document.querySelector('.btn__boost');
+const price = document.querySelector('.price__boost');
+
+
+
+pepe.addEventListener('click', clickTap)
+
+btnBoost.addEventListener('click', clickD)
+
+
+
+function clickTap() {
+    count.textContent++;
+    if (clickD) {
+        let a = 2;
+        let result = parseInt(count.textContent) + a;
+        count.textContent = result;
+    };
+};
+
+
+
+function clickD() {
+    if (parseInt(count.textContent) >= parseInt(price.textContent)) {
+        let result = count.textContent - price.textContent;
+        count.textContent = result;
+    };
+};
+
+xooooooasdf\
+sadfg
