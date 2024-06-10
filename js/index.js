feb0e52ba716426602e6be5b83e87af18d61f586
Ivan Shamin <ivansamin215@gmail.com>
2024-06-10 10:55:27 +0300

    boostTap

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ const count = document.querySelector('.count__click');
 const pepe = document.querySelector('.pepe');
 const btnBoost = document.querySelector('.btn__boost');
 const price = document.querySelector('.price__boost');
-
+let boostTap = false;
 
 
 pepe.addEventListener('click', clickTap)
@@ -13,7 +13,7 @@ btnBoost.addEventListener('click', clickD)
 
 function clickTap() {
     count.textContent++;
-    if (clickD) {
+    if (boostTap == true) {
         let a = 2;
         let result = parseInt(count.textContent) + a;
         count.textContent = result;
@@ -26,5 +26,6 @@ function clickD() {
     if (parseInt(count.textContent) >= parseInt(price.textContent)) {
         let result = count.textContent - price.textContent;
         count.textContent = result;
+        boostTap = true;
     };
 };
